d9a111c9d4410b9afd8a4065d976aab922d6463e
Tartavik <81617514+Tartavik@users.noreply.github.com>
2021-05-27 10:30:05 +0300

    added slider,media,styles

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {arrSliderText} from './sliderText'
+import { arrSliderText } from "./sliderText.js";
 const containerHeaderMenu = document.getElementsByClassName(
   "containerHeaderMenu"
 )[0];
@@ -9,17 +9,77 @@ const headerMenu = document.getElementsByClassName("headerMenu")[0];
 const anchors = document.querySelectorAll('a[href^="#head"');
 const tabs = document.getElementsByClassName("tabs")[0];
 const accordContainer = document.getElementsByClassName("accordContainer");
-const number = document.getElementById("number");
-const accordText = document.getElementById("accordText");
-const accordion = document.getElementById("accordion");
 const headAccordionFaqs = document.getElementsByClassName("headAccordionFaqs");
+const sliderP = document.getElementsByClassName("sliderP")[0];
+const sliderSpan = document.getElementsByClassName("sliderSpan")[0];
+const sliderButton = document.getElementsByClassName("sliderButton");
+const headerInner = document.getElementsByClassName('headerInner')[0];
+let sliderHead = document.getElementsByClassName("sliderHead")[0];
+let newArrSliderText =
+  screen.width >= 1440 ? arrSliderText.slice() : arrSliderText.slice(0, 5);
 let sticky = tabs.offsetTop;
+let stickyMain = headerInner.offsetTop;
+let currentSlider = 0;
+let previousPageYOffset = 0;
 let linkPageYOffset = {
   headTips: document.getElementById("headTips").offsetTop,
   headGlossary: document.getElementById("headGlossary").offsetTop,
   headFAQs: document.getElementById("headFAQs").offsetTop,
 };
-console.log('arrSliderText');
+createAccordionTop(accordContainer);
+createAccordionBottom(headAccordionFaqs);
+createSliderClick(newArrSliderText);
+
+function createSliderClick(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let div = document.createElement("div");
+    let span = document.createElement("span");
+    span.innerText = arr[i].name;
+    div.classList.add("sliderItem");
+    div.appendChild(span);
+    sliderHead.appendChild(div);
+    div.addEventListener("click", changeSliderText);
+    if (i === currentSlider) {
+      div.classList.add("activeSliderLink");
+    }
+  }
+}
+
+function changeSliderText() {
+  Array.from(sliderHead.children).forEach((slider) => {
+    slider.classList.remove("activeSliderLink");
+  });
+  newArrSliderText.forEach((slider, i) => {
+    if (this.children[0].innerText === slider.name) {
+      sliderHead.children[i].classList.add("activeSliderLink");
+      currentSlider = i;
+      console.log(currentSlider);
+      sliderP.innerText = slider.name;
+      sliderSpan.innerText = slider.text;
+    }
+  });
+}
+
+function changeSliderTextArrow() {
+  let nextNumber = this.value === "right" ? 1 : -1;
+  currentSlider += nextNumber;
+  if (currentSlider + nextNumber > newArrSliderText.length) {
+    currentSlider = 0;
+  }
+  if (currentSlider === -1) {
+    currentSlider = newArrSliderText.length - 1;
+  }
+  sliderP.innerText = newArrSliderText[currentSlider].name;
+  sliderSpan.innerText = newArrSliderText[currentSlider].text;
+  Array.from(sliderHead.children).forEach((slider) => {
+    slider.classList.remove("activeSliderLink");
+  });
+  sliderHead.children[currentSlider].classList.add("activeSliderLink");
+}
+
+for (let i = 0; i < sliderButton.length; i++) {
+  sliderButton[i].addEventListener("click", changeSliderTextArrow);
+}
 
 gamburgerContainer.addEventListener("click", () => {
   containerHeaderMenu.classList.toggle("none");
@@ -27,9 +87,6 @@ gamburgerContainer.addEventListener("click", () => {
   gamburger.classList.toggle("backIcon");
   document.body.classList.toggle("overflow");
 });
-
-createAccordionTop(accordContainer);
-createAccordionBottom(headAccordionFaqs);
 
 function createAccordionTop(acc) {
   for (let i = 0; i < acc.length; i++) {
@@ -94,15 +151,31 @@ for (let i = 0; i < headerMenu.children.length; i++) {
 }
 
 window.onscroll = () => {
-  myFunction();
+  let currentPageYOffset = window.pageYOffset;
+  if (currentPageYOffset < previousPageYOffset) {
+    showTabsStickyMenu();
+    showHeaderInnerStickyMenu();
+  } else {
+    tabs.classList.remove("sticky");
+    headerInner.classList.remove("stickyMain");
+  }
+  previousPageYOffset = currentPageYOffset;
   currentTabsLink();
 };
 
-function myFunction() {
+function showTabsStickyMenu() {
   if (window.pageYOffset >= sticky) {
     tabs.classList.add("sticky");
   } else {
     tabs.classList.remove("sticky");
+  }
+}
+
+function showHeaderInnerStickyMenu(){
+  if (window.pageYOffset >= stickyMain + 10) {
+    headerInner.classList.add("stickyMain");
+  } else {
+    headerInner.classList.remove("stickyMain");
   }
 }
 
--- sliderText.js
+++ sliderText.js
@@ -1,67 +1,66 @@
-
 export let arrSliderText = [
-    {
-      name: 'Botnet' ,
-      text: '1Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-    {
-      name: 'Breach',
-      text: '2Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-    {
-      name: 'Cloud',
-      text: '3Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-    {
-      name: 'DDoS' ,
-      text: '4Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-    {
-      name: 'Endpoint',
-      text: '5Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-    {
-      name: 'Domain',
-      text: '6Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-    {
-      name: 'Exploit',
-      text: '7Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-    {
-      name: 'Firewall',
-      text: '8Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-    {
-      name: 'IP Address',
-      text: '9Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-    {
-      name: 'Malware ',
-      text: '10Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-    {
-      name: 'Penetration',
-      text: '11Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-    {
-      name: 'Test',
-      text: '12Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-    {
-      name: ' Phishing/Spearphishing',
-      text: '13Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-    {
-      name: 'Ransomware',
-      text: '14Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-    {
-      name: 'Session',
-      text: '15Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-    {
-      name: 'Hijacking',
-      text: '16Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.'
-    },
-  ];
+  {
+    name: "Botnet",
+    text: "1Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+  {
+    name: "Breach",
+    text: "2Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+  {
+    name: "Cloud",
+    text: "3Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+  {
+    name: "DDoS",
+    text: "4Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+  {
+    name: "Endpoint",
+    text: "5Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+  {
+    name: "Domain",
+    text: "6Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+  {
+    name: "Exploit",
+    text: "7Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+  {
+    name: "Firewall",
+    text: "8Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+  {
+    name: "IP Address",
+    text: "9Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+  {
+    name: "Malware",
+    text: "10Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+  {
+    name: "Penetration",
+    text: "11Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+  {
+    name: "Test",
+    text: "12Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+  {
+    name: "Phishing",
+    text: "13Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+  {
+    name: "Ransomware",
+    text: "14Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+  {
+    name: "Session",
+    text: "15Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+  {
+    name: "Hijacking",
+    text: "16Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, libero.",
+  },
+];
