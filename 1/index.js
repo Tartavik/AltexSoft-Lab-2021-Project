4fc89482b6a80db941223206863ef15a09805ec5
Tartavik <81617514+Tartavik@users.noreply.github.com>
2021-06-16 13:16:20 +0300

    update index.js

--- 1/index.js
+++ 1/index.js
@@ -1,42 +1,34 @@
-import { arrSliderText } from "./sliderText.js";
+import { arrSliderText } from './sliderText.js';
 
-const containerHeaderMenu = document.getElementsByClassName(
-  "container-header-menu"
-)[0];
-const hamburgerContainer = document.getElementsByClassName(
-  "hamburger-container"
-)[0];
-const hamburger = document.getElementsByClassName("hamburger-icon")[0];
-const headerMenu = document.getElementsByClassName("header-menu")[0];
+const containerHeaderMenu = document.querySelector('.container-header-menu');
+const hamburgerContainer = document.querySelector('.hamburger-container');
+const hamburger = document.querySelector('.hamburger-icon');
+const headerMenu = document.querySelector('.header-menu');
 const anchors = document.querySelectorAll('a[href^="#head"');
-const tabs = document.getElementsByClassName("tabs")[0];
-const accordContainer = document.getElementsByClassName("accord-container");
-const headAccordion = document.getElementsByClassName("head-accordion");
-const headAccordionFaqs = document.getElementsByClassName(
-  "head-accordion-Faqs"
-);
-const container = document.getElementsByClassName('container')[0];
-const sliderTitle = document.getElementsByClassName("slider-title")[0];
-const sliderSpan = document.getElementsByClassName("slider-span")[0];
-const sliderButton = document.getElementsByClassName("slider-button");
-const headerInner = document.getElementsByClassName("header-inner")[0];
-let sliderHead = document.getElementsByClassName("slider-head")[0];
+const tabs = document.querySelector('.tabs');
+const accordContainer = document.getElementsByClassName('accord-container');
+const headAccordion = document.getElementsByClassName('head-accordion');
+const headAccordionFaqs = document.getElementsByClassName('head-accordion-Faqs');
+const container = document.querySelector('.container');
+const sliderTitle = document.querySelector('.slider-title');
+const sliderSpan = document.querySelector('.slider-span');
+const sliderButton = document.getElementsByClassName('slider-button');
+const headerInner = document.querySelector('.header-inner');
+let sliderHead = document.querySelector('.slider-head');
 let newArrSliderText;
 let sticky = tabs.offsetTop;
 let stickyMain = headerInner.offsetTop;
 let currentSlider = 0;
 let linkPageYOffset = {
-  headTips: document.getElementById("head-tips"),
-  headGlossary: document.getElementById("head-glossary"),
-  headFAQs: document.getElementById("head-FAQs"),
+  headTips: document.getElementById('head-tips'),
+  headGlossary: document.getElementById('head-glossary'),
+  headFAQs: document.getElementById('head-FAQs'),
 };
 createAccordionTop(headAccordion);
 createAccordionBottom(headAccordionFaqs);
 createSliderClick(arrSliderText);
 
-window.addEventListener(
-  `resize`,
-  (event) => {
+window.addEventListener(`resize`,(event) => {
     sticky = tabs.offsetTop;
     createSliderClick(arrSliderText);
   },
@@ -45,29 +37,29 @@ window.addEventListener(
 
 function createSliderClick(arr) {
   newArrSliderText = screen.width >= 1440 ? arr.slice() : arr.slice(0, 5);
-  sliderHead.innerHTML = "";
+  sliderHead.innerHTML = '';
   for (let i = 0; i < newArrSliderText.length; i++) {
-    let div = document.createElement("div");
-    let span = document.createElement("span");
+    let div = document.createElement('div');
+    let span = document.createElement('span');
     span.innerText = newArrSliderText[i].name;
-    div.classList.add("slider-item");
+    div.classList.add('slider-item');
     span.classList.add('slider-item-text');
     div.appendChild(span);
     sliderHead.appendChild(div);
-    div.addEventListener("click", changeSliderText);
+    div.addEventListener('click', changeSliderText);
     if (i === currentSlider) {
-      div.classList.add("active-slider-link");
+      div.classList.add('active-slider-link');
     }
   }
 }
 
 function changeSliderText() {
   Array.from(sliderHead.children).forEach((slider) => {
-    slider.classList.remove("active-slider-link");
+    slider.classList.remove('active-slider-link');
   });
   newArrSliderText.forEach((slider, i) => {
     if (this.children[0].innerText === slider.name) {
-      sliderHead.children[i].classList.add("active-slider-link");
+      sliderHead.children[i].classList.add('active-slider-link');
       currentSlider = i;
       sliderTitle.innerText = slider.name;
       sliderSpan.innerText = slider.text;
@@ -76,7 +68,7 @@ function changeSliderText() {
 }
 
 function changeSliderTextArrow() {
-  let nextNumber = this.value === "right" ? 1 : -1;
+  let nextNumber = this.value === 'right' ? 1 : -1;
   currentSlider += nextNumber;
   if (currentSlider + nextNumber > newArrSliderText.length) {
     currentSlider = 0;
@@ -87,35 +79,35 @@ function changeSliderTextArrow() {
   sliderTitle.innerText = newArrSliderText[currentSlider].name;
   sliderSpan.innerText = newArrSliderText[currentSlider].text;
   Array.from(sliderHead.children).forEach((slider) => {
-    slider.classList.remove("active-slider-link");
+    slider.classList.remove('active-slider-link');
   });
-  sliderHead.children[currentSlider].classList.add("active-slider-link");
+  sliderHead.children[currentSlider].classList.add('active-slider-link');
 }
 
 for (let i = 0; i < sliderButton.length; i++) {
-  sliderButton[i].addEventListener("click", changeSliderTextArrow);
+  sliderButton[i].addEventListener('click', changeSliderTextArrow);
 }
 
-hamburgerContainer.addEventListener("click", () => {
-  containerHeaderMenu.classList.toggle("is-none");
-  hamburger.classList.toggle("hamburger-icon");
-  hamburger.classList.toggle("back-icon");
-  document.body.classList.toggle("is-hidden");
+hamburgerContainer.addEventListener('click', () => {
+  containerHeaderMenu.classList.toggle('is-none');
+  hamburger.classList.toggle('hamburger-icon');
+  hamburger.classList.toggle('back-icon');
+  document.body.classList.toggle('is-hidden');
 });
 
 function createAccordionTop(acc) {
   for (let i = 0; i < accordContainer.length; i++) {
-    acc[i].addEventListener("click", (e) => {
+    acc[i].addEventListener('click', (e) => {
       console.log(e.currentTarget.nextElementSibling);
       let panel = e.currentTarget.nextElementSibling;
-      e.currentTarget.children[2].classList.toggle("accordion-active");
-      e.currentTarget.children[2].classList.toggle("accordion-button");
-      e.currentTarget.children[0].classList.toggle("is-none");
-      e.currentTarget.children[1].classList.toggle("accord-text-active");
+      e.currentTarget.children[2].classList.toggle('accordion-active');
+      e.currentTarget.children[2].classList.toggle('accordion-button');
+      e.currentTarget.children[0].classList.toggle('is-none');
+      e.currentTarget.children[1].classList.toggle('accord-text-active');
       if (panel.style.maxHeight) {
         panel.style.maxHeight = null;
       } else {
-        panel.style.maxHeight = 800 + "px";
+        panel.style.maxHeight = 800 + 'px';
       }
     });
   }
@@ -123,43 +115,43 @@ function createAccordionTop(acc) {
 
 function createAccordionBottom(acc) {
   for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", (e) => {
+    acc[i].addEventListener('click', (e) => {
       let panel = e.currentTarget.nextElementSibling;
       console.log(e.currentTarget.children[2]);
-      e.currentTarget.children[2].classList.toggle("rotate");
+      e.currentTarget.children[2].classList.toggle('rotate');
       if (panel.style.maxHeight) {
         panel.style.maxHeight = null;
       } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
+        panel.style.maxHeight = panel.scrollHeight + 'px';
       }
     });
   }
 }
 
 for (let anchor of anchors) {
-  anchor.addEventListener("click", function (e) {
+  anchor.addEventListener('click', function (e) {
     e.preventDefault();
     for (let link of anchors) {
-      link.classList.remove("current-tabs-link");
+      link.classList.remove('current-tabs-link');
     }
-    e.currentTarget.classList.add("current-tabs-link");
+    e.currentTarget.classList.add('current-tabs-link');
 
-    const blockID = anchor.getAttribute("href").substr(1);
-    console.log(anchor.getAttribute("href"));
+    const blockID = anchor.getAttribute('href').substr(1);
+    console.log(anchor.getAttribute('href'));
     document.getElementById(blockID).scrollIntoView({
-      behavior: "smooth",
-      block: "start",
+      behavior: 'smooth',
+      block: 'start',
       alignToTop: false,
     });
   });
 }
 
 for (let i = 0; i < headerMenu.children.length; i++) {
-  headerMenu.children[i].addEventListener("click", (e) => {
+  headerMenu.children[i].addEventListener('click', (e) => {
     for (let j = 0; j < headerMenu.children.length; j++) {
-      headerMenu.children[j].classList.remove("active-link");
+      headerMenu.children[j].classList.remove('active-link');
     }
-    e.currentTarget.classList.add("active-link");
+    e.currentTarget.classList.add('active-link');
   });
 }
 
@@ -171,18 +163,18 @@ window.onscroll = () => {
 
 function showTabsStickyMenu() {
   if (window.pageYOffset >= sticky - 75) {
-    tabs.classList.add("sticky");
+    tabs.classList.add('sticky');
   } else {
-    tabs.classList.remove("sticky");
+    tabs.classList.remove('sticky');
   }
 }
 function showheaderInnerStickyMenu() {
   if (window.pageYOffset >= stickyMain + 10 && window.screen.width >= 1200) {
-    headerInner.classList.add("sticky-main");
-    container.classList.add("added-empty-field");
+    headerInner.classList.add('sticky-main');
+    container.classList.add('added-empty-field');
   } else {
-    headerInner.classList.remove("sticky-main");
-    container.classList.remove("added-empty-field");
+    headerInner.classList.remove('sticky-main');
+    container.classList.remove('added-empty-field');
   }
 }
 
@@ -197,10 +189,10 @@ function currentTabsLink() {
     currentLink = linkPageYOffset.headFAQs.id;
   }
   for (let link of anchors) {
-    if (currentLink === link.getAttribute("href").substr(1)) {
-      link.classList.add("current-tabs-link");
+    if (currentLink === link.getAttribute('href').substr(1)) {
+      link.classList.add('current-tabs-link');
     } else {
-      link.classList.remove("current-tabs-link");
+      link.classList.remove('current-tabs-link');
     }
   }
 }
