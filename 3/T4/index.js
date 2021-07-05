1d10c1d1f14551f420f8d7e5f11edfcbf464f721
Tartavik <81617514+Tartavik@users.noreply.github.com>
2021-07-05 15:06:43 +0300

    added T4

---
+++ 3/T4/index.js
@@ -0,0 +1,47 @@
+const min = document.getElementById('min');
+const max = document.getElementById('max');
+const buttonFindFriendlyNumbers = document.getElementById('find');
+
+buttonFindFriendlyNumbers.addEventListener('click',() =>{
+    findFriendlyNumbers(+min.value,+max.value);
+})
+
+function findFriendlyNumbers (min,max) {
+    let arrFriendlyNumbers = [];
+    for (let i = min; i <= max; i++){
+        let arrDividerFirst = getDividerNumber(i);
+        let sumArrDividerF = getSumArrDivider(arrDividerFirst);
+        if(sumArrDividerF < max){
+            let arrDividerSecond = getDividerNumber(sumArrDividerF);
+            let sumArrDividerS = getSumArrDivider(arrDividerSecond);
+            if(i === sumArrDividerS&&sumArrDividerF > sumArrDividerS?true:false){
+                arrFriendlyNumbers.push([i,sumArrDividerF])
+            }
+        }
+    }
+    console.log(arrFriendlyNumbers);
+}
+
+function isInteger (num) {
+ return (num ^ 0) === num;
+}
+
+function getDividerNumber (num) {
+    let arr = [1];
+    let lastNumb = num;
+    for (j = 2; j < lastNumb; j++){
+        let result = num / j;
+        if(isInteger(result)){
+            arr.push(result);
+            arr.push(j);
+            lastNumb = result;
+        }
+    }
+    return arr;
+}
+
+function getSumArrDivider (arr) {
+    return arr.reduce((cur,prev) => {
+        return cur += prev;
+    },0)
+}
